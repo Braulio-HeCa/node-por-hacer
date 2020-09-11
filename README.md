3d3a9b6f8d6833ba1401195486905841254603cb
Braulio Hermida <braulio_hermida@hotmail.com>
2020-09-11 17:47:31 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicación de comandos
+
+Este programa permite crear, actualizar y borrar notas.
+
+Recuerden instalar los paquetes de node
+
+´´´
+npm install
+´´´
---
+++ config/yargs.js
@@ -0,0 +1,42 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+}
+
+const completado = {
+
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea.'
+}
+
+
+
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+
+        descripcion
+    })
+
+.command('actualizar', 'Actualiza el estado de completado de una tarea', {
+
+    descripcion,
+    completado
+})
+
+
+.command('borrar', 'Borra una tarea', {
+
+    descripcion,
+    completado
+})
+
+
+.help()
+    .argv;
+
+module.exports = {
+    argv
+}
